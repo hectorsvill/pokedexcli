62cfb5e387c7ea0674990c7d0993ab487c8dcbaa
hectorsvill <hectorstevenvillasano@gmail.com>
2025-03-25 23:50:05 -0400

    use strings.Fields

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	pokedexcli()
+	// pokedexcli()
+	str1 := "  foo   \bar     baz   "
+	fields1 := strings.Fields(str1)
+	fmt.Println(len(fields1))
+	fmt.Printf("Fields: %q\n", fields1) // Output: Fields: ["foo" "bar" "baz"]
+
 
 }
 
@@ -20,7 +26,7 @@ func pokedexcli() {
 		fmt.Print("pokedexcli > ")
 		scanner.Scan()
 		str := scanner.Text()
-		inputArr := cleanInput(str)
+		inputArr := strings.Fields(str)
 		command := inputArr[0]
 		if cmd, ok := cliCommands[command]; ok {
 			if err := cmd.callback(); err != nil {
--- utils.go
+++ utils.go
@@ -1,18 +1,2 @@
 package main
 
-import (
-	"regexp"
-	"strings"
-)
-
-func removeDoubleSpace(str string) string {
-	re := regexp.MustCompile(`\s+`)
-	return re.ReplaceAllString(str, " ")
-}
-
-func cleanInput(text string) []string {
-	str := strings.ToLower(text)
-	str = strings.Trim(str, " ")
-	str = removeDoubleSpace(str)
-	return strings.Split(str, " ")
-}
