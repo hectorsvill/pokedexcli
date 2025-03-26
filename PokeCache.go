eb78ae27f2a188c8504cfaff3a6583df9c5cc421
hectorsvill <hectorstevenvillasano@gmail.com>
2025-03-26 01:34:02 -0400

    PokeCache

---
+++ PokeCache.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+var PCache PokeCache
+
+type PokeCache struct {
+	cache map[string]CacheEntry
+}
+
+type CacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func (pk PokeCache) Init() {
+	PCache = PokeCache{
+		cache: make(map[string]CacheEntry),
+	}
+}
+
+func (pk PokeCache) Add(url string, entry CacheEntry, mux *sync.RWMutex) {
+	mux.RLock()
+	PCache.cache[url] = entry
+	mux.RUnlock()
+}
+
+func (pk PokeCache) Get(url string, mux *sync.RWMutex) (CacheEntry, error) {
+	mux.RLock()
+	entry := PCache.cache[url]
+	mux.RUnlock()
+	return entry, nil
+}
+
+func (pk PokeCache) Exist(url string) bool {
+	_, ok := PCache.cache[url]
+	if ok {
+		return true
+	}
+	return false
+}
