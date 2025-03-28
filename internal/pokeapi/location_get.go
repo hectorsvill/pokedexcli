293c7b5c5f6243f49a177ca870bd430cf29a530a
hectorsvill <hectorstevenvillasano@gmail.com>
2025-03-27 22:44:33 -0400

    move fx to location_get, use NewRequest, panic on error

--- internal/pokeapi/location_get.go
+++ internal/pokeapi/location_get.go
@@ -1,1 +1,52 @@
 package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+
+
+func (c Client)getLocations(locationsUrl string) []Location {
+	mux := &sync.RWMutex{}
+
+	if PCache.Exist(locationsUrl) {
+		entry, err := PCache.Get(locationsUrl, mux)
+		err = json.Unmarshal(entry.val, &result)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	req, err := http.NewRequest("GET", locationsUrl, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+	
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	entry := CacheEntry{
+		createdAt: time.Now(),
+		val:       data,
+	}
+	go PCache.Add(locationsUrl, entry, mux)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		panic(err)
+	}
+
+	return result.Results
+}
