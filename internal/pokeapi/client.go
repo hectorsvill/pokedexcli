d62b2c10cd622423d6607400b8e813c1d5ace95b
hectorsvill <hectorstevenvillasano@gmail.com>
2025-03-28 02:04:48 -0400

    atach cache to client with New

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,15 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/hectorsvill/pokedexcli/internal/pokecache"
+
 )
 
 type Client struct {
+	cache pokecache.PokeCache
 	httpClient http.Client
 }
 
 func NewClient(timeout time.Duration) Client {
 	return Client{
-		httpClient: http.Client{
+		cache: pokecache.NewPokeCache(),
+		httpClient: http.Client {
 			Timeout: timeout,
 		},
 	}
