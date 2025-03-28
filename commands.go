e2add313d76f182e097dd9402cf2794f12993924
hectorsvill <hectorstevenvillasano@gmail.com>
2025-03-28 02:03:03 -0400

    refactor commands.go for config stuct

--- commands.go
+++ commands.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-	"errors"
+	// "errors"
 	"fmt"
-	"math/rand"
+	// "math/rand"
 	"os"
-	"time"
+	// "time"
+
+	// "github.com/hectorsvill/pokedexcli/internal/pokeapi"
+
 )
 
-var cliCommands map[string]CliCommand
 
 type CliCommand struct {
 	name           string
 	description    string
-	callback       func() error
-	callbackWinput func(string) error
+	callback       func(*config) error
 }
 
-func (cm CliCommand) Init() {
-	cliCommands = map[string]CliCommand{
+func getCliCommands() map[string]CliCommand {
+	return map[string]CliCommand{
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
@@ -34,110 +35,117 @@ func (cm CliCommand) Init() {
 			description: "print next locations",
 			callback:    MapNext,
 		},
-		"mapb": {
-			name:        "mapb",
-			description: "print previous locations",
-			callback:    MapBack,
-		},
-		"explore": {
-			name:        "explore",
-			description: "print pokemon in location",
-			callback:    Explore,
-		},
-		"inspect": {
-			name:        "inspect",
-			description: "print pokemon stats",
-			callback:    Inspect,
-		},
-		"catch": {
-			name:        "catch",
-			description: "try catching a pokemon",
-			callback:    Catch,
-		},
+		// "mapb": {
+		// 	name:        "mapb",
+		// 	description: "print previous locations",
+		// 	callback:    MapBack,
+		// },
+		// "explore": {
+		// 	name:        "explore",
+		// 	description: "print pokemon in location",
+		// 	callback:    Explore,
+		// },
+		// "inspect": {
+		// 	name:        "inspect",
+		// 	description: "print pokemon stats",
+		// 	callback:    Inspect,
+		// },
+		// "catch": {
+		// 	name:        "catch",
+		// 	description: "try catching a pokemon",
+		// 	callback:    Catch,
+		// },
 	}
 }
 
-func commandExit() error {
+func (cmd CliCommand)helpString() string {
+	return fmt.Sprintf("%s: %s", cmd.name, cmd.description)
+}
+
+func commandExit(cfg *config) error {
 	fmt.Println("\nClosing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func (c CliCommand) helpString() string {
-	return fmt.Sprintf("%s: %s", c.name, c.description)
-}
-
-func Usage() error {
+func Usage(cfg *config) error {
 	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
-	for _, cliCommand := range cliCommands {
+	for _, cliCommand := range getCliCommands() {
 		fmt.Println(cliCommand.helpString())
 	}
 	return nil
 }
 
-func MapNext() error {
-	if result == nil {
-		locations := getLocations(locations_url)
-		printLocations(locations)
-	} else {
-		locations := getLocations(result.Next)
-		printLocations(locations)
+func MapNext(cfg *config) error {
+	if len(cfg.inputArr) != 1 {
+		panic("input error")
 	}
-	return nil
-}
+	
+	result := cfg.client.GetLocations(cfg.nextLocation)
 
-func MapBack() error {
-	if result.Previous == "" {
-		locations := getLocations(locations_url)
-		printLocations(locations)
-	} else {
-		locations := getLocations(result.Previous)
-		printLocations(locations)
-	}
-	return nil
-}
+	cfg.nextLocation = result.Next
 
-func Explore() error {
-	if len(InputArr) != 2 {
-		return errors.New("Explore(): input error")
-	}
-	pokemons := getLocation(InputArr[1])
-	fmt.Println()
-	for _, p := range pokemons {
-		fmt.Printf("- %v\n", p.Name)
-	}
-	return nil
-}
-
-func Inspect() error {
-	if len(InputArr) != 2 {
-		return errors.New("Inspect(): input error")
+	for _, location := range result.Results {
+		fmt.Println(location.Name)
 	}
 
-	stats := getStats(InputArr[1])
-	for _, stat := range stats {
-		fmt.Printf("  -%v: %v\n", stat.Name, stat.Base_Stat)
-	}
-	return nil
-}
 
-func Catch() error {
-	if len(InputArr) != 2 {
-		return errors.New("Catch(): input error\n")
-	}
-	pokemon := InputArr[1]
-	fmt.Printf("Throwing a Pokeball at %v...\n", pokemon)
-
-	stats := getStats(InputArr[0])
-	hpBaseStat := stats[0].Base_Stat 
-
-	time.Sleep(500 * time.Millisecond)
-	randVal := rand.Intn(hpBaseStat)
-	if randVal > hpBaseStat/2 - 7  {
-		fmt.Printf("%v was caught!\n", pokemon)
-	} else {
-		fmt.Printf("%v escaped!\n", pokemon)
-	}
 
 	return nil
 }
+
+// func MapBack() error {
+// 	if result.Previous == "" {
+// 		locations := getLocations(locations_url)
+// 		printLocations(locations)
+// 	} else {
+// 		locations := getLocations(result.Previous)
+// 		printLocations(locations)
+// 	}
+// 	return nil
+// }
+
+// func Explore() error {
+// 	if len(InputArr) != 2 {
+// 		return errors.New("Explore(): input error")
+// 	}
+// 	pokemons := getLocation(InputArr[1])
+// 	fmt.Println()
+// 	for _, p := range pokemons {
+// 		fmt.Printf("- %v\n", p.Name)
+// 	}
+// 	return nil
+// }
+
+// func Inspect() error {
+// 	if len(InputArr) != 2 {
+// 		return errors.New("Inspect(): input error")
+// 	}
+
+// 	stats := getStats(InputArr[1])
+// 	for _, stat := range stats {
+// 		fmt.Printf("  -%v: %v\n", stat.Name, stat.Base_Stat)
+// 	}
+// 	return nil
+// }
+
+// func Catch() error {
+// 	if len(InputArr) != 2 {
+// 		return errors.New("Catch(): input error\n")
+// 	}
+// 	pokemon := InputArr[1]
+// 	fmt.Printf("Throwing a Pokeball at %v...\n", pokemon)
+
+// 	stats := getStats(InputArr[0])
+// 	hpBaseStat := stats[0].Base_Stat 
+
+// 	time.Sleep(500 * time.Millisecond)
+// 	randVal := rand.Intn(hpBaseStat)
+// 	if randVal > hpBaseStat/2 - 7  {
+// 		fmt.Printf("%v was caught!\n", pokemon)
+// 	} else {
+// 		fmt.Printf("%v escaped!\n", pokemon)
+// 	}
+
+// 	return nil
+// }
