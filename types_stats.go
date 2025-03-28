28abbd0dc1f7a434ed1a1c28d4f08f7e1bacbcc2
hectorsvill <hectorstevenvillasano@gmail.com>
2025-03-27 22:12:47 -0400

    rename file

---
+++ types_stats.go
@@ -0,0 +1,24 @@
+package main
+
+type StatsResult struct {
+	StatsResult []Stats `json:"stats"`
+}
+
+type Stats struct {
+	Base_Stat int  `json:"base_stat"`
+	Stat      Stat `json:"stat"`
+}
+
+type Stat struct {
+	Name      string `json:"name"`
+	Base_Stat int
+}
+
+func (sr StatsResult) getStats() []Stat {
+	stats := []Stat{}
+	for _, stat := range sr.StatsResult {
+		// stat.Stat.Base_Stat = stat.Base_Stat
+		stats = append(stats, stat.Stat)
+	}
+	return stats
+}
